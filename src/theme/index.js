48d5ff4d32cfeb9df0ee6915b94ef3a5ddc0be48
Teseo15 <brayan.blas.egaovil30@gmail.com>
2021-11-12 22:12:37 -0500

    semana11

---
+++ src/theme/index.js
@@ -0,0 +1,20 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  // colores
+  // fuentes
+  // espacios
+  // sombres
+  palette: {
+    primary: {
+      light: "#ff6333",
+      main: "#ff3d00",
+      dark: "#b22a00",
+    },
+    secondary: {
+      main: "#F5B925",
+    },
+  },
+});
+
+export default theme;
